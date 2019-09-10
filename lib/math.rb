48b03f3f10e9d4aa8557ef23049446bd0c106db7
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-10 17:51:00 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,33 @@
 def addition(num1, num2)
-
+  addition= num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  
 end
 
 def division(num1, num2)
-
 end
 
 def multiplication(num1, num2)
-
 end
 
 def modulo(num1, num2)
-
+num1.modulo(num2)
 end
 
 def square_root(num)
+math.sqrt(num)
 
 end
+
+
+
+
+
+
+
+
+
+
+square_root 81
